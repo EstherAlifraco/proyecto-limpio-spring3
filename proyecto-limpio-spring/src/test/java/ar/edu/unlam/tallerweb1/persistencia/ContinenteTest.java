5f0d1d7e682c09d04747aa15f7dd108a662c5ac1
EstherAlifraco <estheralifraco@gmail.com>
2019-05-07 14:39:41 -0300

    Punto Tres

--- proyecto-limpio-spring/src/test/java/ar/edu/unlam/tallerweb1/persistencia/ContinenteTest.java
+++ proyecto-limpio-spring/src/test/java/ar/edu/unlam/tallerweb1/persistencia/ContinenteTest.java
@@ -18,7 +18,7 @@ import static org.assertj.core.api.Assertions.*;
 
 public class ContinenteTest extends SpringTest{
 	
-	private Pais pais, pais1, pais2, pais3;	
+	private Pais pais, pais1, pais2, pais3, pais4;	
 	private Continente continente, continente1, continente2;
 	private Ciudad ciudad, ciudad1, ciudad2, ciudad3;
 	private Ubicacion ubicacion, ubicacion1, ubicacion2, ubicacion3;
@@ -31,6 +31,7 @@ public class ContinenteTest extends SpringTest{
 		pais1= new Pais();
 		pais2= new Pais();
 		pais3= new Pais();
+		pais4= new Pais();
 		
 		continente= new Continente();
 		continente1= new Continente();
@@ -77,6 +78,62 @@ public class ContinenteTest extends SpringTest{
 		assertThat(listaPaises).hasSize(2);
 	}
 	
-//3) Hacer con junit un test que busque todos los países del continente europeo. 
+//3) Hacer con junit un test que busque todos los países del continente europeo.
+	
+    @Test
+    @Transactional
+    @Rollback(true)
+    @SuppressWarnings("unchecked")
+    public void buscarPaisesContinenteEuropeo() {
+    	
+    	continente.setNombre("Europa");
+    	
+    	continente1.setNombre("America");
+    	
+    	continente2.setNombre("Africa");
+    	
+    	pais.setNombre("España");
+    	pais.setIdioma("español");
+    	pais.setContinente(continente);
+    	
+    	pais1.setNombre("Inglaterra");
+    	pais1.setIdioma("Ingles");
+    	pais1.setContinente(continente);
+    	
+		pais2.setNombre("Irlanda");
+		pais2.setIdioma("ingles");
+		pais2.setContinente(continente);
+		
+		pais3.setNombre("Argentina");
+		pais3.setIdioma("español");
+		pais3.setContinente(continente1);
+		
+		pais4.setNombre("Egipto");
+		pais4.setIdioma("Arabe");
+		pais4.setContinente(continente2);
+    	
+    	getSession().save(continente);
+    	getSession().save(continente1);
+    	getSession().save(continente2);
+    	
+    	getSession().save(pais);
+    	getSession().save(pais1);
+    	getSession().save(pais2);
+    	getSession().save(pais3);
+    	getSession().save(pais4);
+    	
+    	List<Pais> list=
+    			getSession().createCriteria(Pais.class)
+    			.createAlias("continente", "c")
+    			.add(Restrictions.eq("c.nombre", "Europa"))
+    			.list();
+    	
+    	assertThat(list.size()).isEqualTo(3);
+    	
+    		
+    }
+    
+    
+//4- Hacer con junit un test que busque todos los países cuya capital están al norte del trópico de cáncer. 
     	
 }
